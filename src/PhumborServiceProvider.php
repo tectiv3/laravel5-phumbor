b0edd4569b52fccbfca05b50f2ad0f0580d147a3
tectiv3 <tective@gmail.com>
2016-09-08 16:06:28 +0300

    path fix

--- src/PhumborServiceProvider.php
+++ src/PhumborServiceProvider.php
@@ -20,7 +20,7 @@ class PhumborServiceProvider extends ServiceProvider {
 	 */
 	public function boot() {
 		$this->publishes([
-			__DIR__.'/../../config/config.php' => config_path('phumbor.php'),
+			__DIR__.'/config/config.php' => config_path('phumbor.php'),
 		]);
 	}
 
